57ffe0a68884ffae4825441c3ec435924bd677aa
Ayako Nakamoto <109193998+AyakoNakamoto-code@users.noreply.github.com>
2022-07-13 17:54:36 +0900

    inputエリアの処理を変数化

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import "./styles.css";
 
 const onClickAdd = () => {
-  alert();
+  const inputText = document.getElementById("inputText").value;
+  alert(inputText);
 };
 
 document.getElementById("add-button").addEventListener("click", () => {
