7fdc0e3e5f121deb6b11d4ec4c61447293a749ef
Ayako Nakamoto <109193998+AyakoNakamoto-code@users.noreply.github.com>
2022-07-14 16:06:27 +0900

    未完了エリアのlist-item>button(完了と削除)にaddEventlistenerを

--- src/index.js
+++ src/index.js
@@ -16,11 +16,16 @@ const onClickAdd = () => {
   // button(完了)タグ生成
   const complateButton = document.createElement("button");
   complateButton.innerText = "完了";
+  complateButton.addEventListener("click", () => {
+    alert("完了");
+  });
 
   // button(完了)タグ生成
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "削除";
-  console.log(deleteButton);
+  deleteButton.addEventListener("click", () => {
+    alert("削除");
+  });
 
   // liの子要素に各要素を設定
   li.appendChild(pTxt);
