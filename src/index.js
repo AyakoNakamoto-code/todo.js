9751f78d1b7a79cbaa238a9631434cc06abe6192
Ayako Nakamoto <109193998+AyakoNakamoto-code@users.noreply.github.com>
2022-07-14 14:52:39 +0900

    innerTextをtextContentに書き換える

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ const onClickAdd = () => {
   // テキストボックスの値を取得し、初期化する
   const inputText = document.getElementById("inputText").value;
   document.getElementById("inputText").value = "";
-  // console.log(inputText);
   // li生成
   const li = document.createElement("li");
   li.className = "list-row";
 
   // liの中のpタグ生成
-  const pText = document.createElement("p");
-  pText.innerText = inputText;
-  console.log(pText);
+  const pTxt = document.createElement("p");
+  pTxt.textContent = inputText;
+
+  console.log(pTxt);
 };
 
 document
