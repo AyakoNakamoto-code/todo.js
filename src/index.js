d0b1845c6cacdb59e3b395b4186e30f73feceb21
Ayako Nakamoto <109193998+AyakoNakamoto-code@users.noreply.github.com>
2022-07-14 13:38:09 +0900

    li要素に"kist-row"クラスを付与、クリックイベント追加改修

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import "./styles.css";
 
 const onClickAdd = () => {
-  // テキストボックスの値を取得し初期化する
+  // テキストボックスの値を取得し、初期化する
   const inputText = document.getElementById("inputText").value;
-  alert(inputText);
-  document.getElementById("addText").value = "";
+  document.getElementById("inputText").value = "";
+
+  // li生成
+  const li = document.createElement("li");
+  li.className = "list-row";
+  console.log(li);
+
+  // liの中のpタグ生成
+  // const listText = document.createElement("p");
+  // listText.className = "p";
+  // console.log(listText);
 };
 
-document.getElementById("add-button").addEventListener("click", () => {
-  onClickAdd();
-});
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
