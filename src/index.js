3812190ecade105f2aa7084e984810ed9df2fa60
Ayako Nakamoto <109193998+AyakoNakamoto-code@users.noreply.github.com>
2022-07-14 13:49:25 +0900

    エラー　pTextのinnerTextが表示されない

--- src/index.js
+++ src/index.js
@@ -4,14 +4,15 @@ const onClickAdd = () => {
   // テキストボックスの値を取得し、初期化する
   const inputText = document.getElementById("inputText").value;
   document.getElementById("inputText").value = "";
-
+  // console.log(inputText);
   // li生成
   const li = document.createElement("li");
   li.className = "list-row";
 
   // liの中のpタグ生成
-  const p = document.createElement("p");
-  console.log(p);
+  const pText = document.createElement("p");
+  pText.innerText = inputText;
+  console.log(pText);
 };
 
 document
